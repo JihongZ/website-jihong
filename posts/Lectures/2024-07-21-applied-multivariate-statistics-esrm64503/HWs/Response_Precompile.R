e493c575c74762a98510d3d626c8ba750a02b128
jj test <z.john.appleid@gmail.com>
2024-09-17 01:36:21 -0500

    revise lecture04

---
+++ posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/Response_Precompile.R
@@ -0,0 +1,54 @@
+library(haven)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+hw_resp_path <- "posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/Responses/" 
+hw_root_path <- "posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/" 
+assignment_filenames <- list.files(hw_resp_path)
+
+## Homework 0
+assignment_tbl <- readxl::read_xlsx(paste0(hw_resp_path, "ESRM 64503_Homework 0.xlsx"))
+colnames(assignment_tbl)
+hw0_assignment_tbl_clean <- assignment_tbl[, c(2, 10, 13, 16, 6, 19:21)]
+colnames(hw0_assignment_tbl_clean) <- c("Starting_Time", "Name", "Question1_Response", "Question2_Response", "Score_PerHW" , "Question1_Answer", "Question2_Answer", "Comment")
+hw0_assignment_tbl_clean$Name <- toupper(hw0_assignment_tbl_clean$Name)
+hw0_assignment_tbl_clean <- hw0_assignment_tbl_clean |> 
+  mutate(Name = case_when(
+    Name == "SUMAIYA FARZANA" ~ "SUMAIYA FARZANA MISHU",
+    Name == "JENNIFFER ROA" ~ "JENNIFFER ROA LOZANO",
+    TRUE ~ Name
+  )) |> 
+  group_by(Name) |> 
+  mutate(Test_Times_HW0 = n()) |> 
+  filter(Starting_Time == max(Starting_Time))
+
+## Homework 1
+assignment_tbl <- readxl::read_xlsx(paste0(hw_resp_path, "ESRM 64503_Homework 1.xlsx"))
+colnames(assignment_tbl)
+hw1_assignment_tbl_clean <- assignment_tbl[, c(2, 7, 8:13)]
+colnames(hw1_assignment_tbl_clean) <- c("Starting_Time", "Name", "Question1_Response", "Question2_Response", "Score_PerHW", "Question1_Answer", "Question2_Answer", "Comment")
+hw1_assignment_tbl_clean$Name <- toupper(hw1_assignment_tbl_clean$Name)
+### Only keep the lastest record
+hw1_assignment_tbl_clean <- hw1_assignment_tbl_clean |> 
+  mutate(Name = case_when(
+    Name == "SUMAIYA FARZANA" ~ "SUMAIYA FARZANA MISHU",
+    Name == "JENNIFFER ROA" ~ "JENNIFFER ROA LOZANO",
+    TRUE ~ Name
+  )) |> 
+  group_by(Name) |> 
+  mutate(Test_Times_HW1 = n()) |> 
+  filter(Starting_Time == max(Starting_Time))
+
+hw_all_tbl <- hw0_assignment_tbl_clean |> 
+  left_join(hw1_assignment_tbl_clean, by = "Name", suffix = c("_HW0", "_HW1")) |> 
+  mutate(across(everything(), \(x) as.character(x))) |> 
+  pivot_longer(c(ends_with("_HW0"), ends_with("_HW1"))) |> 
+  separate(name, into = c("Variable", "HW"), sep = "_HW") |> 
+  pivot_wider(names_from = "Variable", values_from = "value") |> 
+  mutate(
+    across(starts_with("Question"),\(x) gsub(pattern = "\r\n", replacement = "<br/>", x = x))
+  )
+
+saveRDS(hw_all_tbl, file = paste0(hw_root_path, "ESRM64503_Homework_Combined.rds"))
+
---
+++ posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/app.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(readxl)
+library(shinyjs)
+library(dplyr)
+library(DT)
+
+# hw_root_path <- "posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/" 
+dat <- readRDS("ESRM64503_Homework_Combined.rds")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  # Application title
+  titlePanel("Overall Grade"),
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      textInput("name", "Your Full Name", value = "PATRICK MCCLAIN"), # 
+      actionButton("confirm", "Confirm", 
+                   style="simple", size="sm", color = "warning"),
+      conditionalPanel(
+        condition = "input.confirm == 1",
+        checkboxGroupInput(
+          inputId = "columns_selected",
+          label = "Information:",
+          choices = colnames(dat),
+          selected =  colnames(dat)[1:6]
+        )
+      )
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      DTOutput(outputId = "data1")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  observeEvent(input$confirm, {
+    output$data1 <- DT::renderDT({
+      dat |> 
+        filter(Name == input$name) |> 
+        select(input$columns_selected)
+    }, escape = FALSE)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
