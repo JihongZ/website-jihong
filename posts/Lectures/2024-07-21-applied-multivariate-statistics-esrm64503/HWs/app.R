4d5ad42bed51c7c920a7a9b245aae0561cf53444
jihong <jzhang@uark.edu>
2024-09-18 15:04:15 -0500

    adjust layout for homework system

--- posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/app.R
+++ posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/app.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(DT)
 
 # Publish to https://jihongz.shinyapps.io/ESRM64503-Grading/ 
+# https://jihongzhang.org/posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/Grading_ShinyApp.html
 # hw_root_path <- "posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/" 
 dat <- readRDS("ESRM64503_Homework_Combined.rds")
 
@@ -16,34 +17,52 @@ dat <- dat |>
   
 # Define UI for application that draws a histogram
 ui <-  dashboardPage(
-  dashboardHeader(title = "ESRM 64503"), # Application title
+  skin = "purple",
+  dashboardHeader(title = "ESRM 64503 v0.1", titleWidth = "18%"), # Application title
   dashboardSidebar(
+      width = "18%",
       column(12, 
-        selectInput("name", "Your Full Name", choices = unique(dat$Name)), 
-        textInput(inputId = "code", label = "Your Unique Code", value = ""), 
-        actionButton(inputId = "confirm", label = "Confirm"),
-        selectInput("hw_index", "Which Homework", choices = c("All", "Homework0", "Homework1")), 
+        selectInput("name", label = list(icon("file-signature"), "Select the name"), choices = unique(dat$Name)), 
+        textInput(inputId = "code", label = list(icon("key"), "Enter a unique code"), value = "", placeholder = "4-digit code"), # 4-digit code
+        conditionalPanel(
+          condition = "input.confirm >= 1",
+          verbatimTextOutput(outputId = "code_check_msg")
+        ),
+        actionButton(inputId = "confirm", label = "Confirm", icon = icon("circle-check", class="fa-solid fa-circle-check", style="color: #FFD43B;")),
+        selectInput("hw_index", list(icon("scroll"), "Choose the homework"), choices = c("All", "Homework0", "Homework1")), 
         conditionalPanel( # information box
           condition = "!!input.name",
           checkboxGroupInput(
             inputId = "columns_selected",
             label = "Extra Information:",
-            choices = setdiff(colnames(dat), c("Name", "HW", "Starting_Time", "Code")),
+            choiceNames = c("Question 1 - Reponse",
+                            "Question 2 - Reponse",
+                            "Homework's Score",
+                            "Question 1 - Answer",
+                            "Question 2 - Answer",
+                            "Comment/Feedback",
+                            "Your test times"),
+            choiceValues = setdiff(colnames(dat), c("Name", "HW", "Starting_Time", "Code")),
             selected =  setdiff(colnames(dat), c("Name", "HW", "Starting_Time", "Code"))[1:3]
           )
         )
       )
   ),
   dashboardBody(
+    width = "82%",
     # Show a pieplot
     box(
+      title = "Summary",
       width = 12,
-      plotOutput(outputId = "score_pie")
+      plotOutput(outputId = "score_pie"), # Output piechart of score proportion for each person
+      collapsible = TRUE, collapsed = FALSE
     ),
     # Show a plot of the generated distribution
     box(
+      title = "Detailed information",
       width = 12,
-      DTOutput(outputId = "data1")
+      DTOutput(outputId = "data1"),
+      collapsible = TRUE, collapsed = FALSE
     )
   )
 )
@@ -67,8 +86,14 @@ server <- function(input, output) {
     
     ## Check wether user input code match stored code
     if (stored_code != input$code) {
-      print("You code is incorrect!")
+      updateActionButton(inputId = "confirm", icon = icon("circle-check", class="fa-solid fa-circle-check", style="color: #f82a22;"))
+      output$code_check_msg <- renderText({
+        "Your code is incorrect!"
+      })
     }else{
+      updateActionButton(inputId = "confirm", icon = icon("circle-check", class="fa-solid fa-circle-check", style="color: #63E6BE;"))
+      output$code_check_msg <- renderText({""})
+      
       dat_cleaned <- dat |> 
         ungroup() |> 
         filter(Name == input$name) 
@@ -87,7 +112,7 @@ server <- function(input, output) {
           
           Score_person_withpos <- Score_person |> 
             mutate(assignment = factor(assignment, levels = Score_person$assignment)) |> 
-            arrange(assignment) |> 
+            arrange(desc(assignment)) |> 
             mutate(ypos = cumsum(score) - 0.5*score)
           
           # Basic piechart
